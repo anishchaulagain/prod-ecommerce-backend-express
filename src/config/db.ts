b1318d6625c680b30ec739f5c6b0ca7034774193
Anish Chaulagain <anishchaulagain2058@gmail.com>
2026-02-26 09:01:17 +0545

    modify console error

--- src/config/db.ts
+++ src/config/db.ts
@@ -25,7 +25,7 @@ const connectDB = async () => {
       })
       .catch((error) => {
         cached.promise = null;
-        console.error(`MongoDB Connection Error: ${error}`);
+        console.error(`MongoDB Connection Error, View Here: ${error}`);
         throw error;
       });
   }
